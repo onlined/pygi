4ac36724263cbeb834f510b4b4d7565c3e74faf4
Ekin Dursun <ekindursun@gmail.com>
2016-07-25 18:55:08 +0300

    packaged the project

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='pygi',
+    version='1.0',
+    description='Command line interface using gitignore.io API',
+    url='http://github.com/onlined/pygitignore',
+    author='Ekin Dursun',
+    author_email='ekindursun@gmail.com',
+    license='GPLv3',
+    py_modules=['gitignore'],
+    install_requires=[
+        'python-Levenshtein',
+        'more-itertools',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points = {
+        'console_scripts': ['gi=gitignore:main'],
+    }
+)
